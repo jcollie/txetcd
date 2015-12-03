df0236cc4832d3797fbe823c72520c05abeea606
Jeffrey C. Ollie <jeff@ocjtech.us>
2015-12-03 13:16:08 -0600

    testing

--- txetcd/client.py
+++ txetcd/client.py
@@ -14,26 +14,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import random
-from urllib.parse import urlencode
-
-from dateutil.parser import parse as parse_datetime
-from twisted.python import log
-from twisted.web.client import Agent, readBody, _requireSSL
-from twisted.web.http_headers import Headers
 import json
+from urllib.parse import urlencode
+from dateutil.parser import parse as parse_datetime
 
 from zope.interface import implementer
+
 from twisted.internet import defer
-from twisted.web.iweb import IBodyProducer, IPolicyForHTTPS
 from twisted.internet.ssl import optionsForClientTLS
-from twisted.web.client import _HTTP11ClientFactory
+from twisted.logger import Logger
+from twisted.web.client import Agent, readBody, _requireSSL
 from twisted.web.client import HTTPConnectionPool
+from twisted.web.client import _HTTP11ClientFactory
+from twisted.web.http_headers import Headers
+from twisted.web.iweb import IBodyProducer, IPolicyForHTTPS
 
 @implementer(IBodyProducer)
 class StringProducer(object):
-
     def __init__(self, body):
+        assert(isinstance(body, bytes))
         self.body = body
         self.length = len(body)
 
@@ -47,10 +46,8 @@ class StringProducer(object):
     def stopProducing(self):
         pass
 
-
 class EtcdError(Exception):
     pass
-
 
 class EtcdServerError(EtcdError):
     def __init__(self, **kwargs):
@@ -59,13 +56,11 @@ class EtcdServerError(EtcdError):
         self.message = kwargs.get('message')
         self.cause = kwargs.get('cause')
 
-
 class EtcdResponse(object):
     def __init__(self, action, node, index):
         self.action = action
         self.node = node
         self.index = index
-
 
 class EtcdNode(object):
     directory = False
@@ -75,6 +70,7 @@ class EtcdNode(object):
 
         if 'expiration' in kwargs:
             self.expiration = parse_datetime(kwargs['expiration'])
+            
         else:
             self.expiration = None
 
@@ -82,9 +78,9 @@ class EtcdNode(object):
     def from_response(**kwargs):
         if kwargs.get('dir', False):
             return EtcdDirectory(**kwargs)
+        
         else:
             return EtcdFile(**kwargs)
-
 
 class EtcdFile(EtcdNode):
     def __init__(self, **kwargs):
@@ -122,9 +118,11 @@ class QuietHTTP11ClientFactory(_HTTP11ClientFactory):
     noisy = False
 
 class EtcdClient(object):
+    log = Logger()
+    
     API_VERSION = 'v2'
 
-    def __init__(self, reactor, node=('localhost', 4001), ca=None, cert=None):
+    def __init__(self, reactor, node=('localhost', 2379), ca=None, cert=None):
         self.reactor = reactor
         self.node = node
         self.scheme = 'http'
@@ -137,9 +135,9 @@ class EtcdClient(object):
 
         quietPool = HTTPConnectionPool(reactor, persistent = True)
         quietPool.maxPersistentPerHost = 2
-        #quietPool._factory = QuietHTTP11ClientFactory
+        quietPool._factory = QuietHTTP11ClientFactory
 
-        self.agent = Agent(self.reactor, contextFactory=context, pool=quietPool)
+        self.agent = Agent(self.reactor, contextFactory =c ontext, pool = quietPool)
 
     def _decode_response(self, response):
         def decode(text):
@@ -152,7 +150,7 @@ class EtcdClient(object):
     def _construct_response_object(self, obj, headers):
         if 'errorCode' in obj:
             raise EtcdServerError(**obj)
-        index = int(headers.getRawHeaders('X-Etcd-Index', 0)[0])
+        index = int(headers.getRawHeaders(b'X-Etcd-Index', 0)[0])
         return EtcdResponse(obj['action'], EtcdNode.from_response(**obj['node']), index)
 
     def _format_kwarg(self, key, value):
@@ -164,26 +162,21 @@ class EtcdClient(object):
         else:
             return value
 
-    def _request(self, method, path, params=None, data=None, prefer_leader=False):
-        url = '{scheme}://{host}:{port}/{version}{path}'.format(
-            scheme=self.scheme,
-            host=self.node[0],
-            port=self.node[1],
-            version=self.API_VERSION,
-            path=path,
-        )
+    def _request(self, method, path, query = None, data = None, prefer_leader = False):
+        url = URL(scheme = self.scheme,
+                  host = self.node[0],
+                  port = self.node[1],
+                  path = path,
+                  query = query)
 
+        self.log.debug('Sending {m:} request to {u:}', m = method.decode('iso-8859-1'), u = url.asText())
+        
         headers = None
         body = None
 
-        #if params:
-        #    url = '{}?{}'.format(url, urlencode(params))
-
-        #if data:
-        #    headers = Headers({b'Content-Type': [b'application/x-www-form-urlencoded']})
-        #    body = StringProducer(urlencode(data))
-
-        url = bytes(url.encode('utf-8'))
+        if data:
+            headers = Headers({b'Content-Type': [b'application/x-www-form-urlencoded; charset=utf-8']})
+            body = StringProducer(data)
 
         return self.agent.request(method, url, headers, body)
 
@@ -191,56 +184,72 @@ class EtcdClient(object):
         if not key.startswith('/'):
             raise RuntimeError('keys must start with /')
 
-    def _build_params(self, params, method_kwargs, param_map):
+    def _parse_key(self, key):
+        self._validate_key(key)
+        return key.split('/')[1:]
+
+    def _build_path(self, key):
+        path = [self.API_VERSION, 'keys']
+        path.extend(self.parse_key(key))
+        
+    def _build_query(self, params, method_kwargs, param_map):
         for key in param_map.keys():
             if key in method_kwargs:
                 params[param_map[key]] = self._format_kwarg(key, method_kwargs[key])
 
-        return params
+        return tuple(params.items())
 
+    def _build_data(self, params, method_kwargs, param_map):
+        for key in param_map.keys():
+            if key in method_kwargs:
+                params[param_map[key]] = self._format_kwarg(key, method_kwargs[key])
+
+        return urlencode(params).encode('utf-8')
+    
     def create(self, key, value, **kwargs):
-        self._validate_key(key)
-        path = '/keys{key}'.format(key=key)
-        data = self._build_params({'value': value}, kwargs, {
-            'ttl': 'ttl',
-        })
-        d = self._request(b'POST', path, data=data, prefer_leader=True)
+        path = self._build_path(key)
+        data = self._build_data({'value': value},
+                                kwargs,
+                                {
+                                    'ttl': 'ttl',
+                                })
+        d = self._request(b'POST', path, data = data, prefer_leader = True)
         d.addCallback(self._decode_response)
         return d
 
     def set(self, key, **kwargs):
-        self._validate_key(key)
-        path = '/keys{key}'.format(key=key)
-        data = self._build_params({}, kwargs, {
-            'ttl': 'ttl',
-            'value': 'value',
-            'prev_index': 'prevIndex',
-            'prev_value': 'prevValue',
-            'prev_exist': 'prevExist',
-        })
+        path = self._build_path(key)
+        data = self._build_params({},
+                                  kwargs,
+                                  {
+                                      'ttl': 'ttl',
+                                      'value': 'value',
+                                      'prev_index': 'prevIndex',
+                                      'prev_value': 'prevValue',
+                                      'prev_exist': 'prevExist',
+                                  })
 
-        d = self._request(b'PUT', path, data=data, prefer_leader=True)
+        d = self._request(b'PUT', path, data = data, prefer_leader = True)
         d.addCallback(self._decode_response)
         return d
 
     def delete(self, key):
-        self._validate_key(key)
-        path = '/keys{key}'.format(key=key)
-        d = self._request(b'DELETE', path, prefer_leader=True)
+        path = self._build_path(key)
+        d = self._request(b'DELETE', path, prefer_leader = True)
         d.addCallback(self._decode_response)
         return d
 
     def get(self, key, **kwargs):
-        self._validate_key(key)
-        path = '/keys{key}'.format(key=key)
-        params = self._build_params({}, kwargs, {
-            'sorted': 'sorted',
-            'recursive': 'recursive',
-            'consistent': 'consistent',
-            'wait': 'wait',
-            'wait_index': 'waitIndex',
-        })
-
-        d = self._request(b'GET', path, params=params)
+        path = self._build_path(key)
+        query = self._build_query({},
+                                  kwargs,
+                                  {
+                                      'sorted': 'sorted',
+                                      'recursive': 'recursive',
+                                      'consistent': 'consistent',
+                                      'wait': 'wait',
+                                      'wait_index': 'waitIndex',
+                                  })
+        d = self._request(b'GET', path, query = query)
         d.addCallback(self._decode_response)
         return d
